fd355f3fb8d19707f43ed0ab6bec6e37b9111e59
Nightfall35 <ishmaelgoku@gmail.com>
2025-12-16 04:54:23 -0800

    Gps logic is a bit clunky

--- GPSReader.java
+++ GPSReader.java
@@ -2,9 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Closeable;
 
-public GPSReader(String portName) throws Exception {
-    this.portName = portName;
-    this.setName = setName;
-    this.setDaemon(true); // this prevents JVMM shutdown 
-    this.start();
+public class GPSReader implements Closeable {
+    private volatile double lat = 0.0;
+    private volatile double lon = 0.0;
+    private volatile boolean hashFix = false;
+    private volatile boolean running = true;
+
+    private final String portName;
+
+    public GPSReader(String portName) {
+        this.portName =portName;
+        this.setName ("GPS -READER-THREAD");
+        this.setDaemon(true); // this should block jvmm shutdown 
+        this.start();
+    }
 }
