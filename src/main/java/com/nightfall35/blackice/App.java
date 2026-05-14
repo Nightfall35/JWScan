ba48e8568d90473234444f6e3a64aa4f85bda6fb
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2026-05-14 17:37:06 +0200

    Add initial App class with main method for application entry point

---
+++ src/main/java/com/nightfall35/blackice/App.java
@@ -0,0 +1,13 @@
+package com.nightfall35.blackice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Black Ice!" );
+    }
+}
